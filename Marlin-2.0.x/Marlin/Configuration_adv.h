11808c6ec316b09e40e5b198233b0d05a1e042ec
ihuotui <ihuotui@hotmail.com>
2021-01-10 18:10:45 +0800

    marlin2.0 切换固定版本

--- Marlin-2.0.x/Marlin/Configuration_adv.h
+++ Marlin-2.0.x/Marlin/Configuration_adv.h
@@ -2901,7 +2901,7 @@
 #if EITHER(SPINDLE_FEATURE, LASER_FEATURE)
   #define SPINDLE_LASER_ACTIVE_STATE    LOW    // Set to "HIGH" if the on/off function is active HIGH
   #define SPINDLE_LASER_PWM             true   // Set to "true" if your controller supports setting the speed/power
-  #define SPINDLE_LASER_PWM_INVERT      true  // Set to "true" if the speed/power goes up when you want it to go slower
+  #define SPINDLE_LASER_PWM_INVERT      false  // Set to "true" if the speed/power goes up when you want it to go slower
 
   #define SPINDLE_LASER_FREQUENCY       2500   // (Hz) Spindle/laser frequency (only on supported HALs: AVR and LPC)
 
