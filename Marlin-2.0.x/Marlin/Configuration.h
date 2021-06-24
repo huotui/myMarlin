cc0ed6ac89baea9a654215dcdf77a5014cb1b310
ihuotui <ihuotui@hotmail.com>
2021-06-24 22:29:03 +0800

    回中速度变慢

--- Marlin-2.0.x/Marlin/Configuration.h
+++ Marlin-2.0.x/Marlin/Configuration.h
@@ -1401,8 +1401,8 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_XY (40*60)
-#define HOMING_FEEDRATE_Z  (2*60)
+#define HOMING_FEEDRATE_XY (30*60)
+#define HOMING_FEEDRATE_Z  (1*60)
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
